069b9985d128b03911b43a7c01d8ab46042c1926
ashish-ad <ashishad14@gmail.com>
2022-01-16 22:05:43 +0530

    Multiline comments using heredocs

--- firstScript.sh
+++ firstScript.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/zsh 
-
+<<LINUXHINT_MESSAGE
 echo "My favourite movie is \"Limitless\"" > ./file.txt
 
-echo "My favourtie book is think and grow rich" #>> ./file.txt
+echo "My favourtie book is think and grow rich" >> ./file.txt
+LINUXHINT_MESSAGE
+
 
 echo  "My favorite SUV is range rover"  >>  ./file.txt
 
