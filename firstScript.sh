b7a9c35bb7d3e635e76b77b3cd2570b927962756
ashish-ad <ashishad14@gmail.com>
2022-01-17 00:17:33 +0530

    Simple Variable Usage

--- firstScript.sh
+++ firstScript.sh
@@ -1,12 +1,3 @@
 #! /usr/bin/zsh 
-<<LINUXHINT_MESSAGE
-echo "My favourite movie is \"Limitless\"" > ./file.txt
-
-echo "My favourtie book is think and grow rich" >> ./file.txt
-LINUXHINT_MESSAGE
-
-
-echo  "My favorite SUV is range rover"  >>  ./file.txt
-
-
-
+VAR="Hello World"
+echo $VAR
