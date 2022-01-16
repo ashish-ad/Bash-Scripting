ed52b15d7d9961223fac4f61a8f871c1fbb49457
ashish-ad <ashishad14@gmail.com>
2022-01-16 21:18:48 +0530

    comments from middle of line

--- firstScript.sh
+++ firstScript.sh
@@ -2,5 +2,9 @@
 
 echo "My favourite movie is \"Limitless\"" > ./file.txt
 
-echo "My favourtie book is think and grow rich" >> ./file.txt
+echo "My favourtie book is think and grow rich" #>> ./file.txt
+
+echo  "My favorite SUV is range rover"  >>  ./file.txt
+
+
 
