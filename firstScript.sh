676779f130da2b614db61d3157b0693ee7789da6
ashish-ad <ashishad14@gmail.com>
2022-01-16 20:53:32 +0530

    Append to file inside script

--- firstScript.sh
+++ firstScript.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/zsh 
 
-echo "My favourite movie is \" Limitless \"" > ./file.txt
+echo "My favourite movie is \"Limitless\"" > ./file.txt
 
-echo "My favourtie book is think and grow rich" > ./file.txt
+echo "My favourtie book is think and grow rich" >> ./file.txt
+
